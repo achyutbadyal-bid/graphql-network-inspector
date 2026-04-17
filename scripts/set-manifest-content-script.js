338a3fe56dcbad0f3946389bf96e3eeb3902b0c2
Achyut Badyal <achyut.badyal@balkan.id>
2026-04-17 13:47:39 +0530

    fix: clean build artifacts before bundling and make content script URL update optional

--- scripts/set-manifest-content-script.js
+++ scripts/set-manifest-content-script.js
@@ -5,9 +5,11 @@ const manifestPath = "./build/manifest.json"
 const run = async () => {
   const json = await fs.promises.readFile(manifestPath, "utf-8")
   const manifest = JSON.parse(json)
-  manifest.content_scripts[0].matches = [
-    `${process.env.REACT_APP_SHARE_TARGET_URL}/draft?*`,
-  ]
+  if (process.env.REACT_APP_SHARE_TARGET_URL) {
+    manifest.content_scripts[0].matches = [
+      `${process.env.REACT_APP_SHARE_TARGET_URL}/draft?*`,
+    ]
+  }
   await fs.promises.writeFile(manifestPath, JSON.stringify(manifest, null, 2))
 }
 run()
